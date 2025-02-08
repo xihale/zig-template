0b096ef31bf636af13ef070f96e75dbdc219b2c6
xihale <x@xihale.top>
2025-02-08 11:02:59 +0800

    fix: build.zig

--- build.zig
+++ build.zig
@@ -5,13 +5,12 @@ pub fn build(b: *std.Build) void {
 
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe_mod = b.createModule(.{
+    const exe = b.addExecutable(.{
+        .name = "template",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
-
-    const exe = b.addExecutable(.{ .name = "template", .root_module = exe_mod });
 
     const check_step = b.step("check", "check if it complies");
     check_step.dependOn(&exe.step);
@@ -30,7 +29,9 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const exe_unit_tests = b.addTest(.{
-        .root_module = exe_mod,
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
     });
 
     const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
